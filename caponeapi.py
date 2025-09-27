1cd33243e4a3ba6c91aa30a25b03e432257dec10
Grace Paul <gracep24@vt.edu>
2025-09-27 15:49:00 -0400

    adding script for api

---
+++ caponeapi.py
@@ -0,0 +1,12 @@
+import requests
+API_KEY = '614d45c57c1537311a4a3d53121b0492'
+url = f"http://api.nessieisreal.com/branches?key={API_KEY}"
+#url = f"http://api.nessieisreal.com/accounts"
+response = requests.get(url)
+data = response.json()
+if response.status_code == 200:
+    print("Branches table:")
+    print(data)
+else:
+    print("Error:", response.status_code, response.text)
+    #http://api.nessieisreal.com/enterprise/customers?key=614d45c57c1537311a4a3d53121b0492
